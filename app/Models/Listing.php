5ae19c141d5f79f53a787bef332480085f601798
bishalshrestha13 <biscro7@gmail.com>
2022-08-13 22:58:37 +0545

    added add,edit,delete listings functionality + user login registration functionality

--- app/Models/Listing.php
+++ app/Models/Listing.php
@@ -9,6 +9,8 @@ class Listing extends Model
 {
     use HasFactory;
 
+    protected $fillable = ['title','company','location','website','email','description','tags','logo','user_id'];
+
     public function scopeFilter($query, array $filters){
         if($filters['tag'] ?? false){
             $query->where('tags','like','%'.request('tag').'%');
@@ -17,5 +19,10 @@ class Listing extends Model
         if($filters['search'] ?? false){
             $query->where('title','like','%'.request('search').'%')->orWhere('description','like','%'.request('search').'%')->orWhere('tags','like','%'.request('search').'%');
         }
+    }
+
+    // Relationship To User
+    public function user(){
+        return $this->belongsTo(User::class,'user_id');
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,10 @@
 <?php
 
-use App\Http\Controllers\ListingController;
 use App\Models\Listing;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\ListingController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,6 +17,41 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+// All Listings
 Route::get('/',[ListingController::class,'index']);
+
+// Show Create Form
+Route::get('/listings/create',[ListingController::class,'create'])->middleware('auth');
+
+// Store Listing Data
+Route::post('/listings',[ListingController::class,'store'])->middleware('auth');
+
+// Show Edit Form
+Route::get('/listings/{listing}/edit',[ListingController::class,'edit'])->middleware('auth');
+
+//Update Listing
+Route::put('/listings/{listing}',[ListingController::class,'update'])->middleware('auth');
+
+//Delete Listing
+Route::delete('/listings/{listing}',[ListingController::class,'destroy'])->middleware('auth');
+
+// Manage Listings
+Route::get('/listings/manage',[ListingController::class,'manage'])->middleware('auth');
+
+// Single Listing
 Route::get('/listings/{listing}',[ListingController::class,'show']);
 
+// show register/create Form
+Route::get('/register',[UserController::class,'create'])->middleware('guest');
+
+// create New user
+Route::post('/users',[UserController::class,'store']);
+
+// Log user Out
+Route::post('/logout',[UserController::class,'logout'])->middleware('auth');
+
+// Show loging Form
+Route::get('/login',[UserController::class,'login'])->name('login')->middleware('guest');
+
+// Login User
+Route::post('/users/authenticate',[UserController::class,'authenticate']);
